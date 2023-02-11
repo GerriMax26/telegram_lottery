f146aa3d91e7d87052b930aafe1d4899af83931c
Garaev Maksim <garaev.maxim2013@yandex.ru>
2023-02-11 19:07:06 +0300

    create db

---
+++ db.py
@@ -0,0 +1,13 @@
+from getpass import getpass
+from mysql.connector import connect,Error
+
+try:
+    with connect(
+        host = 'localhost',
+        user = input('Имя пользователя: '),
+        password = getpass('Пароль: '),
+        database = 'lottery',
+    ) as connection:
+        print(connection)
+except Error as e:
+    print(e)
