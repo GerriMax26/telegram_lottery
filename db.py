d1234536b2a284528b39f793c23d8260dbad0623
Garaev Maksim <garaev.maxim2013@yandex.ru>
2023-02-12 19:19:48 +0300

    add DB user

--- db.py
+++ db.py
@@ -1,14 +1,34 @@
 from getpass import getpass
 from mysql.connector import connect,Error
+import mysql.connector
 
-try:
-    with connect(
-        host = 'localhost',
-        user = input('Имя пользователя: '),
-        password = getpass('Пароль: '),
-        database = 'lottery',
-    ) as connection:
-        print(connection)
-except Error as e:
-    print(e)
+class Database:
+    def __init__(self):
+        self.mydb = mysql.connector.connect(
+            host = 'localhost',
+            user = input('Имя пользователя: '),
+            password = getpass('Пароль: '),
+            database = 'lottery',
+        )
+        self.cursor = self.mydb.cursor()
+
+        
+        
+
+    def user_exists(self,id_user):
+        with self.mydb:
+            self.cursor.execute(f'select * from users where id_user = {id_user}')
+            result = self.cursor.fetchall()
+            return bool(len(result))
     
+    def add_user(self,id_user,lang):
+        with self.mydb:
+            sql = "INSERT INTO users (id_user,lang) VALUES (%s, %s)"
+            val = (id_user,lang)
+            self.cursor.execute(sql, val)
+            self.mydb.commit()
+        
+        
+    def get_lang(self,id_user):
+        with self.mydb:
+            return self.cursor.execute('select lang from users where id_user = ?', (id_user)).fetchone()[0]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiogram import Bot,Dispatcher,executor,types
 import markups as nav
 from translation import translation_text
+from db import Database
 
 with open('C:/Users/GaraevMaksim/Desktop/token.txt','r') as file:
     TOKEN_API = file.readline()
@@ -9,26 +10,40 @@ bot = Bot(TOKEN_API)
 
 dp = Dispatcher(bot)
 
-
+db = Database()
 
 
 @dp.message_handler(commands=['start']) 
 async def start_command(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Choise language:',
+    
+    if not db.user_exists(message.from_user.id):
+        
+        await bot.send_message(message.from_user.id, 
+                               'Choise language:',
                          reply_markup=nav.lang_menu)
-    await message.delete()
+
 
 
 @dp.callback_query_handler(text_contains = 'lang_')
 async def set_language(callback: types.CallbackQuery):
+    
     await bot.delete_message(callback.from_user.id, callback.message.message_id)
-    lang = callback.data[5:]
+    
+    if not db.user_exists(callback.from_user.id):
+        
+        lang = callback.data[5:]
+        
+        db.add_user(callback.from_user.id,
+                    lang)
+        
     await bot.send_message(callback.from_user.id, 
                            translation_text('Привет!',lang),
                            reply_markup=nav.main_menu(lang))
 
+
 @dp.message_handler()
 async def mess(message: types.Message):
+    
     await bot.send_message(message)
 
 
