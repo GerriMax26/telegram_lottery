e77611a2cffd391b182e438238292fd51b94b59d
Garaev Maksim <garaev.maxim2013@yandex.ru>
2023-02-13 20:37:30 +0300

    add translation

--- translation.py
+++ translation.py
@@ -3,13 +3,25 @@ translations = {
         'Привет!':'Hello!',
         'Регистрация': 'Registration',
         'Покинуть лотерею':'Leave the lottery',
-        'Ваше Имя и Фамилия?':'Your first and last name?'
+        'Ваше Имя и Фамилия?':'Your first and last name?',
+        'Ваша страна?':'Your country?',
+        'Ваш город?':'Your city?',
+        'Ваш адрес?':'Your address?',
+        'Ваш индеĸс?':'Your postal code?',
+        'Ваш номер телефона?':'Your phone number?',
+        'Ваш адрес электронной почты?':'Your email address?'
     },
     'es':{
         'Привет!': 'Hola!',
         'Регистрация': 'Registro',
         'Покинуть лотерею':'Abandonar la lotería',
-        'Ваше Имя и Фамилия?':'¿Su nombre y apellido?'
+        'Ваше Имя и Фамилия?':'¿Su nombre y apellido?',
+        'Ваша страна?':'¿Su país?',
+        'Ваш город?':'¿Tu ciudad?',
+        'Ваш адрес?':'¿Su dirección?',
+        'Ваш индеĸс?':'¿Su indie Postal?',
+        'Ваш номер телефона?':'¿Su número de Teléfono?',
+        'Ваш адрес электронной почты?':'¿Su dirección de correo electrónico?'
     }
 }
 
