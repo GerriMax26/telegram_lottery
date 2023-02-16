1f93aec474bf8cd9532686998811dfe72271cd1d
Garaev Maksim <garaev.maxim2013@yandex.ru>
2023-02-16 17:30:56 +0300

    add file send_message_for_admin

---
+++ send_message_for_admin.py
@@ -0,0 +1,21 @@
+#Методы для отправки оповещений админу
+# id_admin = 473666786
+
+
+def withdraw_money():
+    pass
+
+def registration():
+    pass
+
+def byu_ticket():
+    pass
+
+def size_prize_user():
+    pass
+
+def jackpot_size():
+    pass
+
+def leave_bot_user():
+    pass
